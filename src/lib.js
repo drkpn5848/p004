e57d3d11ab3c85bada0e9bafff60446adbcbfd91
drkpn5848 <drkpn.in@gmail.com>
2026-01-31 10:36:24 +0530

    v1

---
+++ src/lib.js
@@ -0,0 +1,22 @@
+export const APIURL = "https://jsonplaceholder.typicode.com/users";
+
+export function callApi(reqMethod, url, data, responseHandler)
+{
+    let options;
+    if(reqMethod === "GET" || reqMethod === "DELETE")
+        options = {method: reqMethod, headers:{'Content-Type':'application/json'}};
+    else
+        options = {method: reqMethod, headers:{'Content-Type':'application/json'}, body: data};
+
+    fetch(url, options)
+        //send request in async
+        .then((response)=>{
+            if(!response.ok)
+                throw new Error(response.status + '-' + response.statusText);
+            return response.json();
+        })
+        //receive response
+        .then((res)=>responseHandler(res))
+        //handle error
+        .catch((err)=>alert(err));
+}
